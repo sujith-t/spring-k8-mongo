e81db5676ad36e9a1838dcfd6232cdae4d1bd5ee
Sujith Thillayampalam <sujith.t@informaticsint.com>
2022-01-01 16:46:02 +0530

    jenkins script

---
+++ jenkins.sh
@@ -0,0 +1,11 @@
+cd /opt/jenkins/spring-k8-mongo
+git pull origin dev
+image_version="v-`date +"%s"`"
+mvn clean install -DskipTests=true
+echo "Building and pushing docker image version: $image_version \n"
+docker build -t sujitht/crypto-k8-service:"$image_version" .
+docker login
+docker push sujitht/crypto-k8-service:"$image_version"
+echo "Doing the rollout deployment with image version: $image_version \n"
+sed -i --regexp-extended "s/v\-[0-9]{10}/$image_version/g" crypto-deployment.yml
+kubectl apply -f crypto-deployment.yml
